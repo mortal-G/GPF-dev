1d75f9e591c0904930889a7a4102128b57f09afd
mortal-G <2639682042@qq.com>
2019-07-16 09:45:07 +0800

    dev

--- src/main/java/com/chinasoft/forum/controller/JumpController.java
+++ src/main/java/com/chinasoft/forum/controller/JumpController.java
@@ -1,7 +1,11 @@
 package com.chinasoft.forum.controller;
 
+import com.chinasoft.forum.dal.entity.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class JumpController {
@@ -12,7 +16,9 @@ public class JumpController {
     }
 
     @RequestMapping("updateProfile")
-    public String toUpdateProfile(){
+    public String toUpdateProfile(ModelMap modelMap, HttpServletRequest request){
+        User user = (User)request.getSession().getAttribute("User");
+        modelMap.put("User",user);
         return "updateProfile";
     }
 }
